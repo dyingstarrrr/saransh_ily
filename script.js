14ae2925fe662efc4911e35ff7410039475113b7
dyingstarrrr <usageonlyme234@gmail.com>
2025-02-13 00:07:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,15 +55,15 @@
 */
 const messages = [
     "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "Pakka vaala sure??",
+    "Tum aisa karogi mere saath?",
+    "Maano naaaaa",
+    "O meri pyaari saru",
+    "O meri sundar saruuu",
+    "O meri cutu saruuuuuu",
+    "Nahi maanegi tu kutti saali?",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, haan boldo! ❤️"
 ];
 
 let messageIndex = 0;
